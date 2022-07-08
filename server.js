f654c6ac501d8e08f54129f7b5451466a921bb91
enajera1 <eriknajera013@gmail.com>
2022-07-08 09:47:32 -0500

    push heroku

--- server.js
+++ server.js
@@ -3,15 +3,12 @@ const express = require("express");
 const session = require("express-session");
 const exphbs = require("express-handlebars");
 // const routes = require("./Controllers/index");
-const helpers = require("./Utilities/helpers");
-const sequelize = require("./Config/connection");
-const SequelizeStore = require("connect-session-sequelize")(session.Store);
 
 const app = express();
-// app.listen(process.env.PORT || 3001);
 const PORT = process.env.PORT || 3001;
 
-const hbs = exphbs.create({ helpers });
+const sequelize = require("./Config/connection");
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
 
 const sess = {
   secret: "super secret",
@@ -20,23 +17,26 @@ const sess = {
   saveUninitialized: true,
   store: new SequelizeStore({
     db: sequelize,
-  }),
+  })
 };
+
+const helpers = require("./Utilities/helpers");
 
 app.use(session(sess));
 
+const hbs = exphbs.create({ helpers });
 // Inform Express.js which template engine we're using
 app.engine("handlebars", hbs.engine);
 app.set("view engine", "handlebars");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-console.log(path.join(__dirname, "public"));
+// console.log(path.join(__dirname, "public"));
 app.use(express.static(path.join(__dirname, "public")));
 
 // app.use(routes);
 app.use(require("./Controllers/"));
 
 sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log("Now listening" + PORT));
+  app.listen(PORT, () => console.log("Now listening on port " + PORT));
 });
