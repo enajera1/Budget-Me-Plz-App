d760cc3de537df75ece7f92f635f88c437b5b720
enajera1 <eriknajera013@gmail.com>
2022-07-08 10:02:15 -0500

    heroku push

--- server.js
+++ server.js
@@ -35,7 +35,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
 // app.use(routes);
-app.use(require("./Controllers/"));
+app.use(require("./Controllers"));
 
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log("Now listening on port " + PORT));
